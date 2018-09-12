22eee06c6dbc87d91efe47e8bb440597ed491729
Dmytro Orikhovskyi <dimaoag@gmail.com>
2018-09-12 12:01:52 +0300

    first commit

---
+++ frontend/components/Debug.php
@@ -0,0 +1,14 @@
+<?php
+namespace frontend\components;
+
+class Debug {
+
+    public static function debug($array, $die = false){
+        echo '<pre>';
+        print_r($array);
+        echo '</pre>';
+        if ($die){
+            die();
+        }
+    }
+}
